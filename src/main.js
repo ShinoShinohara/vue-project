54b7e63f43a2e3bda7675e13f276f4c2a2f215f5
shinoshinohara <vshinohara@outlook.com>
2022-09-25 04:35:50 -0400

    adding axios bootstrap

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import Vue from "vue";
 import App from "./components/App.vue";
+import BootstrapVue from "bootstrap-vue";
+import axios, { Axios } from "axios";
+import VueAxios from "vue-axios";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
+import "./assets/style.scss";
+
 Vue.config.productionTip = false;
+
+Vue.use(BootstrapVue);
+Vue.use(VueAxios, axios);
 
 new Vue({
   router,
