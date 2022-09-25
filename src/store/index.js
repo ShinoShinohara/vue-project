d3cf362c812ebf689b3af8648cf09c6b353c2a22
shinoshinohara <vshinohara@outlook.com>
2022-09-25 05:09:23 -0400

    add false token

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,32 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    user: {
+      id: null,
+      email: "",
+      username: "",
+    },
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
+    CURRENT_USER_FETCHED(state, user) {
+      state.user.id = user.id;
+      state.user.email = user.email;
+      state.user.username = user.username;
+    },
   },
   actions: {
+    async initialLoad(context) {
+      if (localStorage.bgtrackerjwt) {
+        Vue.axios.defaults.headers.common.Authorization = `Bearer ${localStorage.bgtrackerjwt}`;
+      }
+      const res = await Vue.axios.get("/api/auth/currentUser");
+      context.commit("CURRENT_USER_FETCHED", res.data.user);
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
